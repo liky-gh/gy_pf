d02026b020c8ba01879760e4c0e851249a77aa9c
liky12 <gayoung0703@naver.com>
2021-01-04 17:09:40 +0900

    save1

---
+++ js/myscript.js
@@ -0,0 +1,29 @@
+$(".topbt").on("click",function(){
+    $("body,html").animate({scrollTop:"0px"},750);
+})
+
+
+
+$(".homebt").on("click",function(){
+    $("body,html").animate({scrollTop:"0px"},750);
+})
+
+$(".aboutbt").on("click",function(){
+    $("body,html").animate({scrollTop:"630px"},750);
+})
+
+$(".skillbt").on("click",function(){
+    $("body,html").animate({scrollTop:"1400px"},750);
+})
+
+$(".prbt").on("click",function(){
+    $("body,html").animate({scrollTop:"2230px"},750);
+})
+
+$(".aebt").on("click",function(){
+    $("body,html").animate({scrollTop:"3260px"},750);
+})
+
+$(".webbt").on("click",function(){
+    $("body,html").animate({scrollTop:"4240px"},750);
+})
