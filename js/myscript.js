9f79432a748cd560eef603dac6c94813f9313fd7
liky12 <gayoung0703@naver.com>
2021-02-26 09:52:01 +0900

    1

--- js/myscript.js
+++ js/myscript.js
@@ -9,19 +9,19 @@ $(".homebt").on("click",function(){
 })
 
 $(".aboutbt").on("click",function(){
-    $("body,html").animate({scrollTop:"630px"},750);
+    $("body,html").animate({scrollTop:"1120px"},750);
 })
 
 $(".skillbt").on("click",function(){
-    $("body,html").animate({scrollTop:"1400px"},750);
+    $("body,html").animate({scrollTop:"1850px"},750);
 })
 
 $(".prbt").on("click",function(){
-    $("body,html").animate({scrollTop:"2230px"},750);
+    $("body,html").animate({scrollTop:"2700px"},750);
 })
 
 $(".aebt").on("click",function(){
-    $("body,html").animate({scrollTop:"3260px"},750);
+    $("body,html").animate({scrollTop:"3700px"},750);
 })
 
 $(".webbt").on("click",function(){
